ca87ded1e0d7f0a1cf3f6bda1128a3a869eff749
Abraham Yusuf <abraham.yusuf@deepset.ai>
2024-11-23 01:32:00 +0100

    fix

--- deepset-cloud-api/src/endpoints/workspace.rs
+++ deepset-cloud-api/src/endpoints/workspace.rs
@@ -1,31 +1,19 @@
 use apisdk::{send, ApiResult};
-use serde::{Deserialize, Serialize};
 use tracing::instrument;
 
 use crate::types::Workspace;
 use crate::DeepsetCloudApi;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct WorkspaceList {
-    root: Vec<Workspace>,
-}
-
 impl DeepsetCloudApi {
     #[instrument]
-    pub async fn get_workspace(&self, name: String) -> ApiResult<Workspace> {
+    pub async fn get_workspace(&self, name: &str) -> ApiResult<Workspace> {
         let req = self.get(format!("/api/v1/workspaces/{}", name)).await?;
-        send!(req).await
+        send!(req, Json).await
     }
 
     #[instrument]
     pub async fn list_workspaces(&self) -> ApiResult<Vec<Workspace>> {
-        let workspace_list = self.list_workspaces_().await?;
-        Ok(workspace_list.root)
-    }
-
-    #[instrument]
-    async fn list_workspaces_(&self) -> ApiResult<WorkspaceList> {
         let req = self.get("/api/v1/workspaces").await?;
-        send!(req).await
+        send!(req, Json).await
     }
 }
